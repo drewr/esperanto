25d610378662440d29197077adec9a7471d94732
Drew Raines <aaraines@gmail.com>
2011-06-23 16:39:32 -0500

    Remove node-client.

--- src/esperanto/client.clj
+++ src/esperanto/client.clj
@@ -1,25 +1,16 @@
 (ns esperanto.client
-  (:require [cheshire.core :as json])
   (:import (org.elasticsearch.common.settings ImmutableSettings)
            (org.elasticsearch.common.transport InetSocketTransportAddress)
            (org.elasticsearch.client.transport TransportClient)
            (org.elasticsearch.node NodeBuilder)))
 
-(defn node-client
-  "Contruct a node-client from a clojure map of settings"
-  [settings]
-  (-> (NodeBuilder/nodeBuilder)
-      (.loadConfigSettings false)
-      (.settings (-> (ImmutableSettings/settingsBuilder)
-                     (.loadFromSource (json/encode settings))
-                     (.put "transport.tcp.compress" true)))
-      (.node)
-      (.client)))
-
 (defn make-node [& {:as settings}]
   (let [s (doto (ImmutableSettings/settingsBuilder)
-            (.put (or settings {})))]
-    (-> (NodeBuilder/nodeBuilder) (.settings s) .build)))
+            (.put (merge settings {"transport.tcp.compress" true})))]
+    (-> (NodeBuilder/nodeBuilder)
+        (.loadConfigSettings false)
+        (.settings s)
+        .build)))
 
 (defn make-client-node [& {:as settings}]
   (apply make-node (-> (merge settings {"node.client" "true"})
