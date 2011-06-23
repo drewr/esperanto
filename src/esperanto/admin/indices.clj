88d9db3d46383c9830ef4b8c02585c4295f3ba0f
Drew Raines <aaraines@gmail.com>
2011-06-23 16:31:02 -0500

    Clean up indices requests.

--- src/esperanto/admin/indices.clj
+++ src/esperanto/admin/indices.clj
@@ -2,14 +2,11 @@
   (:import (org.elasticsearch.client Requests)))
 
 (defn status [client & indices]
-  (future
-    (-> client .admin .indices
-        (.status (Requests/indicesStatusRequest
-                  (make-array String 0)))
-        .actionGet)))
-
-(defn delete-index [client idx]
   (-> client .admin .indices
-      (.delete (Requests/deleteIndexRequest idx))
-      .actionGet))
+      (.status (Requests/indicesStatusRequest
+                (into-array String indices)))))
+
+(defn delete [client idx]
+  (-> client .admin .indices
+      (.delete (Requests/deleteIndexRequest idx))))
 
