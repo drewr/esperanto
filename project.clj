940c02cd34a6567d3f7c1ec1063ac0711b439b55
Drew Raines <aaraines@gmail.com>
2011-09-01 21:46:41 -0500

    elasticsearch-0.17.6.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  "http://download.java.net/maven/2/"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.elasticsearch/elasticsearch "0.17.0-SNAPSHOT"]
+                 [org.elasticsearch/elasticsearch "0.17.6"]
                  [cheshire "2.0.0"]]
   :dev-dependencies [[swank-clojure "1.3.1"]]
   :aot [esperanto.interactive])
