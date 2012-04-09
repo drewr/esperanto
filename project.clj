7ecf61e273e17399a804564aca5b3b51bab9e0c2
Drew Raines <drew@raines.me>
2012-04-08 21:06:39 -0500

    clojure-1.4.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject esperanto "1.0.0-SNAPSHOT"
+(defproject com.draines/esperanto "1.0.0-SNAPSHOT"
   :description "You know, for clojure"
   :repositories {"sonatype"
                  "http://oss.sonatype.org/content/repositories/releases/"
                  "java.net"
                  "http://download.java.net/maven/2/"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0-beta5"]
                  [org.clojure.contrib/repl-utils "1.3.0-alpha4"]
                  [org.elasticsearch/elasticsearch "0.17.6"]
                  [cheshire "2.0.2"]
