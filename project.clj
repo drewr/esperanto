b662baa3d8e1054116c877f9324fc85c4c54e470
Drew Raines <aaraines@gmail.com>
2012-08-01 20:58:08 -0500

    clj-http-0.5.1, cheshire-4.0.1

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  "http://download.java.net/maven/2/"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.elasticsearch/elasticsearch "0.19.2"]
-                 [clj-http "0.3.6"]
+                 [clj-http "0.5.1"]
                  [slingshot "0.10.2"]
-                 [cheshire "4.0.0"]]
+                 [cheshire "4.0.1"]]
   :aot [esperanto.interactive])
