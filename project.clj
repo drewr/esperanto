4edbdaee896164de7658b0067535698395acf253
Drew Raines <aaraines@gmail.com>
2012-08-01 20:20:43 -0500

    clojure-1.4.0.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  "http://oss.sonatype.org/content/repositories/releases/"
                  "java.net"
                  "http://download.java.net/maven/2/"}
-  :dependencies [[org.clojure/clojure "1.4.0-beta5"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.elasticsearch/elasticsearch "0.19.2"]
                  [clj-http "0.3.6"]
                  [slingshot "0.10.2"]
