57bd724d70c8c07da76d7c54c4136ad32f19458c
Drew Raines <aaraines@gmail.com>
2013-02-28 15:47:32 -0600

    Update deps.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  "java.net"
                  "http://download.java.net/maven/2/"}
   :dependencies [[org.clojure/clojure "1.5.0-RC16"]
-                 [org.elasticsearch/elasticsearch "0.20.5"]
+                 [org.elasticsearch/elasticsearch "0.90.0.Beta2-SNAPSHOT"]
+                 [org.fusesource/sigar "1.6.4"]
                  [clj-http "0.5.1"]
                  [slingshot "0.10.2"]
                  [cheshire "4.0.1"]]
